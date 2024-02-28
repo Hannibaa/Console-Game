8eda54e2d92c49334e5da2e6d16f1c0809f8d8d7
Kadda Aoues <houes_k@yahoo.fr>
2024-02-28 04:14:25 +0300

    added more generic function for collision detection between  any object that have methode get_bounds() and get_position() and set_position().

--- Console_Game/sample_console_games/Console_utility_1.hpp
+++ Console_Game/sample_console_games/Console_utility_1.hpp
@@ -1,10 +1,6 @@
 #pragma once
-#include <thread>
-#include <vector>
-#include <Windows.h>
-#include <wchar.h>
-#include <algorithm>
-#include <MyLib/Console_Library/Unicode_table.h>
+
+#include "CConsole.h"
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// 
@@ -25,7 +21,14 @@
 #define InScreen(x,y,l,w)   ((x > -1 && x < l) && (y > -1 && y < w ))
 #define _Is_Arithmetic(T)   static_assert(std::is_arithmetic_v<T>, "Type should be arithmetic type")
 
+
 namespace cu {
+
+	// ********************** console ****************************...//
+	cu::Console console;
+	// ********************** console ****************************...//
+
+
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
     // 
@@ -76,6 +79,9 @@ namespace cu {
 		return Point2d<T>{ lhv.x - rhv.x, lhv.y - rhv.y };
 	}
 
+
+	///  Rectangle object
+
 	template<typename T>
 	struct Rect {
 		static_assert(std::is_arithmetic_v<T>, "Type T should be arithmetic type");
@@ -102,22 +108,27 @@ namespace cu {
 		{}
 
 		bool contain(T x, T y) const {
-			return (x >= left && x <= left + width) &&
-				   (y >= top && y <= top + height);
+			return (x >= left && x < left + width) &&
+				   (y >= top && y < top + height);
 		}
 
-		bool contain(const Rect<T>& rect) {
-			return contain(left, top) && contain(left + width, top)
-				&& contain(left, top + height) && contain(left + width, top + height);
+		bool contain(const Rect<T>& rect) const {
+			return contain(rect.left, rect.top) || 
+				   contain(rect.left + rect.width - 1, rect.top)|| 
+				   contain(rect.left, rect.top + rect.height - 1)|| 
+				   contain(rect.left + rect.width - 1, rect.top + rect.height - 1);
 		}
 
 	};
 
+	///////// Rectangle 
 	typedef Rect<float>    fRect;
 	typedef Rect<int>      iRect;
 	typedef Point2d<float> fPoint2d;
 	typedef Point2d<int>   iPoint2d;
 
+
+	/////// Method check point x,y if it's interiour rectangle.
 	template<typename T>
 	bool IsPointInRect(const Point2d<T>& p, const Rect<T>& rect)    // Condition in  
 	{
@@ -129,6 +140,43 @@ namespace cu {
 		return false;
 	}
 
+	// second one 
+	template<typename T>
+	bool IsPointInRect(T x, T y, const Rect<T>& rect) {
+		return IsPointInRect(Point2d<T>(x, y), rect);
+	}
+
+	// Overlap two int particule
+	template<typename P1, typename P2>
+	bool Overlap(const P1& p1, const P2& p2) {
+		auto pos1 = p1.get_position();
+		auto pos2 = p2.get_position();
+
+		return (pos2.x - 1 <= pos1.x && pos1.x <= pos2.x + 1) &&
+			(pos2.y - 1 <= pos1.y && pos1.y <= pos2.y + 1);
+	}
+
+	// Overloap
+	template<typename O1, typename O2>
+	bool is_collid(O1& p1, O2& p2) {
+
+		if (p1.get_bounds().contain(p2.get_bounds())) return true;
+		return false;
+	}
+
+	template<typename O1, typename O2>
+	void collision_process(O1& q1, O2& q2) {
+
+		auto p1 = q1.get_position();
+		auto p2 = q2.get_position();
+		float lx = int(p1.x - p2.x) < 0 ? -1.0 : int(p1.x - p2.x) == 0 ? 0.f : 1.f;
+		float ly = int(p1.y - p2.y) < 0 ? -1.0 : int(p1.y - p2.y) == 0 ? 0.f : 1.f;
+		q1.set_position(p1.x + lx / 2.f, p1.y + ly / 2.f);
+		q2.set_position(p2.x - lx / 2.f, p2.y - ly / 2.f);
+
+	}
+
+
 	bool get_out() {
 
 		if (KeyOn(VK_ESCAPE)) return true;
@@ -137,146 +185,33 @@ namespace cu {
 	}
 
 
-
-	class IConsole {
-	public:
-		virtual void construct_console() const = 0;
-
-		virtual void draw() const = 0;
-
-		virtual ~IConsole() {}
-
-	};
-
-	class DConsole {
-
-	public:
-
-		DConsole(int l, int w)
-			: iscrLength{ l },
-			iscrWidth{ w }
-		{}
-
-		virtual void set_parameter() = 0;
-
-		int  iscrLength;
-		int  iscrWidth;
-
-	};
-
-
-
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// 
-	//    Class that handle Console construction of screen object 'wchar_t' 
-	//    1. create screen object
-	//    2. set parameter of the object. <color><widht,heigh[or length]>
-	// 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	class Console : public IConsole, private DConsole {
-	public:
-		Console(int length, int width)
-			: DConsole(length, width)
-		{
-			// 1. allocate space for screen pointer in type wchar_t 
-			screen = new wchar_t[iscrLength * iscrLength];
-
-			// 2. fill up screen with blank
-			wmemset(screen, L' ', iscrLength * iscrWidth);
-
-			// 3. Create screen buffer handle
-			hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL,
-				CONSOLE_TEXTMODE_BUFFER, NULL);
-
-			// 4. Set console screen buffer handle to actual console
-			SetConsoleActiveScreenBuffer(hConsole);
-
-			// Call this function to write in screen buffer handler.
-			DWORD dwByteWritten = 0;
-			WriteConsoleOutputCharacter(hConsole, screen, iscrLength * iscrWidth, { 0,0 }, &dwByteWritten);
-		}
-
-		// buffer function object 
-		void operator()(int i, int j, wchar_t w) {
-			if ((i > -1 && i < iscrLength) && (j > -1 && j < iscrWidth))
-				screen[i + j * iscrLength] = w;
-		}
-
-		virtual void set_parameter() override {
-
-		}
-
-		constexpr auto get_width() const {
-			return this->iscrWidth;
-		}
-
-		constexpr auto get_length() const {
-			return this->iscrLength;
-		}
-
-		// drawing in screen 
-		virtual void draw() const override {
-			DWORD dwByteWritten = 0;
-			WriteConsoleOutputCharacter(hConsole, screen, iscrLength * iscrWidth, { 0,0 }, &dwByteWritten);
-
-		}
-
-		// clear screen
-		void clear_screen(wchar_t char_fill = L' ') {
-			wmemset(screen, char_fill, iscrLength * iscrWidth);
-		}
-
-		~Console() {
-			delete[] screen;
-			//SetConsoleMode(hConsoleIn, dwOldMode);
-			//CloseHandle(hConsoleIn);
-			CloseHandle(hConsole);
-
-		}
-
-	private:
-		void construct_console() const {
-
-		}
-
-
-		HANDLE    hConsole;
-		wchar_t* screen;
-
-	};
-
-
-
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 
 //    Functions
 // 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
-	void draw_hline(cu::Console& console, int y, int x0, int x1, wchar_t c = L'\x2588') {
+	void draw_hline(int y, int x0, int x1, wchar_t c = L'\x2588') {
 
 		for (int k = x0; k != x1 + 1; ++k)
 			console(k, y, c);
 	}
 
-	void draw_vline(cu::Console& console, int x, int y0, int y1, wchar_t c = L'\x2588') {
+	void draw_vline(int x, int y0, int y1, wchar_t c = L'\x2588') {
 
 		for (int k = y0; k != y1 + 1; ++k)
 			console(x, k, c);
 	}
 
-	void draw_rectangle(cu::Console& console, const cu::iRect& rect, wchar_t c = L'\x2588') {
+	void draw_rectangle(const cu::iRect& rect, wchar_t c = L'\x2588') {
 
-		draw_hline(console, rect.top, rect.left, rect.left + rect.width, c);
-		draw_hline(console, rect.top + rect.height, rect.left, rect.left + rect.width, c);
+		draw_hline(rect.top, rect.left, rect.left + rect.width, c);
+		draw_hline(rect.top + rect.height, rect.left, rect.left + rect.width, c);
 
-		draw_vline(console, rect.left, rect.top, rect.top + rect.height, c);
-		draw_vline(console, rect.left + rect.width, rect.top, rect.top + rect.height, c);
+		draw_vline(rect.left, rect.top, rect.top + rect.height, c);
+		draw_vline(rect.left + rect.width, rect.top, rect.top + rect.height, c);
 	}
 
-	void print_text_at(Console& console, const std::wstring& wstr, int x, int y) {
+	void print_text_at(const std::wstring& wstr, int x, int y) {
 		for (int j = 0; j < wstr.size(); ++j)
 		{
 			console(x + j, y, wstr[j]);
@@ -284,6 +219,7 @@ namespace cu {
 	}
 
 	std::pair<int, int> get_boxed_text_dimension(const std::wstring& text, wchar_t rl = L'\n') {
+
 		int max = 0;
 		int lines = 0;
 
@@ -295,10 +231,11 @@ namespace cu {
 		}
 
 		return std::pair{ max, lines };
+
 	}
 
 	// Print the text inside the box
-	void print_text_in_box(Console& console, int x, int y, const std::wstring text, wchar_t rl = L'\n') {
+	void print_text_in_box(int x, int y, const std::wstring text, wchar_t rl = L'\n') {
 
 		for (int i = 0; i < text.size(); ++i) {
 			std::wstring str;
@@ -308,13 +245,13 @@ namespace cu {
 				if (i >= text.size()) break;
 			}
 
-			print_text_at(console, str, x, y++);
+			print_text_at(str, x, y++);
 			str.clear();
 		}
 	}
 
-
-	void message_box(Console& console, int x, int y, const std::wstring& sTitle, const std::wstring& sBody)
+	// message box adem
+	void message_box(int x, int y, const std::wstring& sTitle, const std::wstring& sBody)
 	{
 		// making box for message 
 		/* 
@@ -335,21 +272,21 @@ namespace cu {
 		console(x + 1, y, whLine);
 		console(x + 2, y, whLine);
 		console(x + 3, y, wBracket0);
-		print_text_at(console, sTitle, x + 4  , y);
+		print_text_at(sTitle, x + 4  , y);
 		console(x0 + 1 , y, wBracket1);
-		draw_hline(console, y, x0 + 2 , x + 2 + max_l , whLine);
+		draw_hline(y, x0 + 2 , x + 2 + max_l , whLine);
 		console(x + 3 + max_l, y ,  wBox01);
 
 		// two vertical lines 2 and 3 :
-		draw_vline(console, x            , y+1, y + lines , wvLine);
-		draw_vline(console, x + 3 + max_l, y+1, y + lines , wvLine);
+		draw_vline(x            , y+1, y + lines , wvLine);
+		draw_vline(x + 3 + max_l, y+1, y + lines , wvLine);
 
 		// last horizontal line 4 :
 		console(x, y + lines + 1, wBox10);
-		draw_hline(console, y + lines + 1, x + 1, x + 2 + max_l, whLine);
+		draw_hline(y + lines + 1, x + 1, x + 2 + max_l, whLine);
 		console(x + 3 + max_l, y + lines + 1, wBox11);
 
 		// put the text inside box 
-		print_text_in_box(console, x + 1, y + 1, sBody);
+		print_text_in_box(x + 1, y + 1, sBody);
 	}
 }
